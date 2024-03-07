d5449a4e1b13193b388b37fa5c528239536f9263
Eric <487737744@qq.com>
2024-03-07 21:27:55 +0800

    modify(path): Adjust the path of the file related to the page.

--- packages/client/pages/general/general.tsx
+++ packages/client/pages/general/general.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function General(): React.ReactElement {
-  return <div></div>;
+  return <div>General Setting</div>;
 }
--- packages/client/pages/main.tsx
+++ packages/client/pages/main.tsx
@@ -5,8 +5,9 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import { MenuContainer } from "client/module/menu";
 import { ModalContainer } from "client/module/modal";
 import Editor from "./editor/editor";
-import Home from "./views/home/home";
-import Update from "./views/update/update";
+import Home from "./home/home";
+import Update from "./update/update";
+import General from "./general/general";
 
 function Main(): ReactElement {
   useKeyboardRegister();
@@ -18,6 +19,7 @@ function Main(): ReactElement {
       <Routes>
         <Route path="/" element={<Navigate to="/home" />}></Route>
         <Route path="home/*" element={<Home />} />
+        <Route path="general/*" element={<General />} />
         <Route path="editor/*" element={<Editor />} />
         <Route path="update" element={<Update />} />
       </Routes>
